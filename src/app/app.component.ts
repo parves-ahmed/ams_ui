d3b097f45eabf9d4bb2b18a84a67df5029e79729
parves <parves5405@gmail.com>
2022-04-28 16:34:52 +0600

    28-04-2022 hide nav and head

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'workflow-ui';
+  title = 'ams-ui';
+  showHead: boolean = false;
+  constructor(private router: Router){
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/login') {
+          this.showHead = false;
+        } else {
+          // console.log("NU")
+          this.showHead = true;
+        }
+      }
+    });
+  }
 }
